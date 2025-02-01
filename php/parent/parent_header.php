d845796291e6fc128852bc8147e8d7c4e511b2c4
katerynakkuzmanova <katerynakuzmanova@gmail.com>
2025-02-02 00:44:44 +0200

    коментари parent_header.php

--- php/parent/parent_header.php
+++ php/parent/parent_header.php
@@ -1,5 +1,6 @@
 <?php
-if (isset($message)) {
+if (isset($message)) { // Проверяваме дали има съобщения за показване
+   // Обхождаме всички съобщения и ги показваме на страницата
    foreach ($message as $message) {
       echo '
       <div class="message">
@@ -11,11 +12,14 @@ if (isset($message)) {
 }
 ?>
 
+<!-- Хедър секцията на страницата -->
 <header class="header">
    <div class="header-2">
       <div class="flex">
+         <!-- Лого и навигация за родителите -->
          <a href="parent.php" class="logo">Parent Navigation</a>
 
+         <!-- Навигационно меню -->
          <nav class="navbar">
             <a href="parent.php">main</a>
             <a href="calendar.php">calendar</a>
@@ -24,17 +28,19 @@ if (isset($message)) {
             <a href="profile.php">personal data</a>
          </nav>
 
+         <!-- Икони за мобилно меню и потребител -->
          <div class="icons">
             <div id="menu-btn" class="fas fa-bars"></div>
             <div id="user-btn" class="fa-solid fa-circle-user"></div>
          </div>
 
+         <!-- Падащо меню за потребителската информация -->
          <div class="user-box">
             <p>username : <span>
-                  <?php echo $_SESSION['parent_name']; ?>
+                  <?php echo $_SESSION['parent_name']; ?> <!-- Показваме потребителското име -->
                </span></p>
-            <p>email : <span><?php echo $_SESSION['parent_email']; ?></span></p>
-            <a href="../common/logout.php" class="delete-btn">logout</a>
+            <p>email : <span><?php echo $_SESSION['parent_email']; ?></span></p> <!-- Показваме имейла на потребителя -->
+            <a href="../common/logout.php" class="delete-btn">logout</a> <!-- Бутон за изход от профила -->
          </div>
       </div>
    </div>

2a64607747d8f69d4b211671b789f548327a9751
katerynakkuzmanova <katerynakuzmanova@gmail.com>
2025-02-02 14:38:30 +0200

    коментари calendar.php

--- php/parent/calendar.php
+++ php/parent/calendar.php
@@ -2,17 +2,19 @@
 // Календар за родителя
 // Родителят трябва да може да вижда календар с всички събития за децата си, както и да добавя коментари към тези събития.
 
-session_start();
-include '../common/config.php';
-include '../common/functions.php';
-include 'parent_header.php'; 
+session_start(); // Стартираме сесията, за да имаме достъп до сесийните променливи
+include '../common/config.php'; // Включваме конфигурационния файл, който съдържа връзката с базата данни
+include '../common/functions.php'; // Включваме файл с допълнителни функции
+include 'parent_header.php'; // Включваме заглавната част за родителите
 
-if (!isset($_SESSION['parent_id'])) {
+if (!isset($_SESSION['parent_id'])) { // Проверяваме дали родителят е влязъл в системата
+    // Ако не е влязъл, го пренасочваме към страницата за вход
     header('Location: login.php');
     exit();
 }
 
-$parentId = $_SESSION['parent_id'];
+$parentId = $_SESSION['parent_id']; // Вземаме ID на родителя от сесията
+// Взимаме списъка с деца на родителя чрез функцията getChildrenByParent()
 $children = getChildrenByParent($parentId);
 ?>
 
@@ -22,9 +24,12 @@ $children = getChildrenByParent($parentId);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Parent Calendar</title>
+    <!-- Включваме стилове за FullCalendar -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css" rel="stylesheet" />
+    <!-- Включваме външни CSS файлове -->
     <link rel="stylesheet" href="../../css/style.css">
     <link rel="stylesheet" href="../../css/calendar.css">
+    <!-- Включваме jQuery и FullCalendar библиотеките -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
@@ -33,6 +38,7 @@ $children = getChildrenByParent($parentId);
 <body>
     <h1>Calendar for children activities</h1>
 
+    <!-- Падащо меню за избор на дете -->
     <div class="child-select">
         <label for="child">Choose a child:</label>
         <select id="child" name="child">
@@ -42,11 +48,13 @@ $children = getChildrenByParent($parentId);
         </select>
     </div>
 
+    <!-- Див за показване на календара -->
     <div id="calendar"></div>
 
     <script>
         $(document).ready(function() {
             function loadCalendar(childId) {
+                // Унищожаваме предишния календар (ако има такъв) и зареждаме нов с данните за избраното дете
                 $('#calendar').fullCalendar('destroy');
                 $('#calendar').fullCalendar({
                     header: {
@@ -55,11 +63,13 @@ $children = getChildrenByParent($parentId);
                         right: 'month,agendaWeek,agendaDay'
                     },
                     events: function(start, end, timezone, callback) {
+                        // Изпращаме AJAX заявка за зареждане на събитията от get_events.php
                         $.ajax({
                             url: 'get_events.php',
                             method: 'GET',
                             data: { childId: childId },
                             success: function(response) {
+                                // Парсираме върнатите събития и ги подаваме на календара
                                 callback(JSON.parse(response));
                             }
                         });
@@ -67,9 +77,11 @@ $children = getChildrenByParent($parentId);
                 });
             }
 
+            // Зареждаме календара с първото дете от списъка (ако има такова
             var firstChild = $('#child').val();
             if (firstChild) loadCalendar(firstChild);
 
+            // При промяна на избраното дете се презарежда календарът
             $('#child').change(function() {
                 loadCalendar($(this).val());
             });

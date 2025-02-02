269fcf94d7ed8fdd20b2b4403ebd4a971e498e0c
katerynakkuzmanova <katerynakuzmanova@gmail.com>
2025-02-02 14:55:15 +0200

    коментари babysitter_header.php

--- php/babysitter/babysitter_header.php
+++ php/babysitter/babysitter_header.php
@@ -1,5 +1,7 @@
 <?php
+// Проверяваме дали има съобщения за показване
 if (isset($message)) {
+    // Обхождаме всички съобщения и ги извеждаме на страницата
     foreach ($message as $message) {
         echo '
         <div class="message">
@@ -11,11 +13,14 @@ if (isset($message)) {
 }
 ?>
 
+<!-- Хедър секцията на страницата -->
 <header class="header">
     <div class="header-2">
         <div class="flex">
+            <!-- Лого и навигация за детегледачи -->
             <a href="babysitter.php" class="logo">Babysitter Navigation</a>
 
+            <!-- Навигационно меню -->
             <nav class="navbar">
                 <a href="babysitter.php">main</a>
                 <a href="calendar.php">calendar</a>
@@ -23,17 +28,19 @@ if (isset($message)) {
                 <a href="kids.php">kids</a>
                 <a href="profile.php">personal data</a>
             </nav>
+            <!-- Икони за мобилно меню и профил на потребителя -->
             <div class="icons">
-                <div id="menu-btn" class="fas fa-bars"></div>
-                <div id="user-btn" class="fa-solid fa-circle-user"></div>
+                <div id="menu-btn" class="fas fa-bars"></div> <!-- Бутон за мобилно меню -->
+                <div id="user-btn" class="fa-solid fa-circle-user"></div> <!-- Бутон за отваряне на потребителското меню -->
             </div>
 
+            <!-- Падащо меню за потребителската информация -->
             <div class="user-box">
                 <p>username: <span>
-                    <?php echo $_SESSION['babysitter_name']; ?>
+                    <?php echo $_SESSION['babysitter_name']; ?> <!-- Показване на потребителското име -->
                 </span></p>
-                <p>email: <span> <?php echo $_SESSION['babysitter_email']; ?> </span></p>
-                <a href="../common/logout.php" class="delete-btn">logout</a>
+                <p>email: <span> <?php echo $_SESSION['babysitter_email']; ?> </span></p> <!-- Показване на имейла -->
+                <a href="../common/logout.php" class="delete-btn">logout</a> <!-- Бутон за изход от профила -->
             </div>
         </div>
     </div>
